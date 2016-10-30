2440626377ba63c044f2260e679562297f90dbae
Skylar Schipper <ss@schipp.co>
2016-10-29 23:08:53 -0700

    Initial commit

---
+++ SkyKit/SkyKit.h
@@ -0,0 +1,19 @@
+//
+//  SkyKit.h
+//  SkyKit
+//
+//  Created by Skylar Schipper on 10/29/16.
+//  Copyright © 2016 Skylar Schipper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SkyKit.
+FOUNDATION_EXPORT double SkyKitVersionNumber;
+
+//! Project version string for SkyKit.
+FOUNDATION_EXPORT const unsigned char SkyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkyKit/PublicHeader.h>
+
+
